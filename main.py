4ba45f32ad81d419067615e9a8c80f43aebb5ced
Róbert Palša <ZeroSystem97@users.noreply.github.com>
2025-04-10 13:13:34 +0200

    Add Exception
    Added try block to catch KeyError exception and ask the user for prompt again.

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import pandas
 nato_phonetic_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dic = {row.letter:row.code for (index, row) in nato_phonetic_alphabet.iterrows()}
 
-word = input("Enter a word: ").upper()
-code_list = [alphabet_dic[letter] for letter in word]
-print(code_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        code_list = [alphabet_dic[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(code_list)
+
+generate_phonetic()
